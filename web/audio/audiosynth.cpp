f1e5faacf28e3f38bf7548765c01efda10270c16
augustin64 <me.git@augustin64.fr>
2025-06-24 20:54:08 +0100

    why did last workflow run fail ? I changed nothing..

--- web/audio/audiosynth.cpp
+++ web/audio/audiosynth.cpp
@@ -129,7 +129,7 @@ template<typename T> T depromisify(deto::async::Promise<T> promise) {
     });
 
     while (!completed) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
     if (completed != 1) { throw std::runtime_error(err_msg); }
     return result;
