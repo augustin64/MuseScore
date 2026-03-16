0e9987f05e1e4bb69c1c736a5f71634edc05da00
augustin64 <me.git@augustin64.fr>
2026-03-16 23:01:57 +0100

    Typo

--- src/notation/internal/mscnotationwriter.cpp
+++ src/notation/internal/mscnotationwriter.cpp
@@ -84,7 +84,7 @@ Ret MscNotationWriter::write(INotationPtr notation, io::IODevice& device, const 
         // fix "ASSERT FAILED!" in isOpenModeReadable()
         // `buf` opened as `IODevice::WriteOnly`, see src/engraving/infrastructure/mscwriter.cpp#L265
         msczWriter.close(); // do some cleanups, e.g. add the end tag
-        buf.open(IODevice::ReadWrite);
+        device.open(IODevice::ReadWrite);
     }
 
     if (!msczWriter.open()) {
